4b5253191981c69712d7dff1b8b38588ecf8081a
ShameemudheenMP <shameemmanalaya26@gmail.com>
2023-03-25 14:04:33 +0530

    bluetooth message working successfully

--- lib/bluetooth.dart
+++ lib/bluetooth.dart
@@ -70,6 +70,49 @@ class BluetoothOffScreen extends StatelessWidget {
 class FindDevicesScreen extends StatelessWidget {
   const FindDevicesScreen({Key? key}) : super(key: key);
 
+  void discoverServices(BluetoothDevice device) async {
+    BluetoothCharacteristic characteristic;
+    print("Discovering services------------------------------------- ");
+    // print("Sleeping");
+    // sleep(const Duration(seconds: 10));
+    List<BluetoothService> servs = await device.discoverServices();
+    servs.forEach(
+      (service) {
+        if (service.uuid.toString() == "4fafc201-1fb5-459e-8fcc-c5c9c331914b") {
+          // Replace XXXX with the UUID of the service
+          service.characteristics.forEach(
+            (c) {
+              if (c.uuid.toString() == "beb5483e-36e1-4688-b7f5-ea07361b26a8") {
+                // Replace YYYY with the UUID of the characteristic
+                print("!!!!!!!!!!Characteristic is matched");
+                characteristic = c;
+                subscribeToCharacteristic(characteristic);
+              }
+            },
+          );
+        }
+      },
+    );
+    // print('Service not found');
+  }
+
+  // Subscribe to the characteristic to receive notifications
+  void subscribeToCharacteristic(BluetoothCharacteristic characteristic) async {
+    await characteristic.setNotifyValue(true);
+    characteristic.value.listen(
+      (value) async {
+        // Handle the received message
+        String message = String.fromCharCodes(value);
+        print("message is : $message");
+        if (message == 'accident') {
+          // sleep(const Duration(seconds: 5));
+          await characteristic.write(utf8.encode('Response'));
+          print('!!!!!!!!!!Response sent');
+        }
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -96,8 +139,11 @@ class FindDevicesScreen extends StatelessWidget {
                             stream: d.state,
                             initialData: BluetoothDeviceState.disconnected,
                             builder: (c, snapshot) {
-                              if (snapshot.data ==
-                                  BluetoothDeviceState.connected) {
+                              if (snapshot.data == BluetoothDeviceState.connected) {
+                                // call the discoverService 
+                                // print("Discovering Services()...................");
+                                discoverServices(d);
+                                
                                 return ElevatedButton(
                                   child: const Text('OPEN'),
                                   onPressed: () => Navigator.of(context).push(
@@ -279,7 +325,7 @@ class _DeviceScreenState extends State<DeviceScreen> {
         initialData: BluetoothDeviceState.connecting,
         builder: (context, snapshot) {
           if (snapshot.data == BluetoothDeviceState.connected) {
-            discoverServices();
+            // discoverServices();
           }
           return Center(
             child: Text(
